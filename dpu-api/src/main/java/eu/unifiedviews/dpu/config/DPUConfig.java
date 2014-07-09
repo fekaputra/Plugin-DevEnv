5696c58c63169fb3d96cd06627d038792f694a13
Sveto Krchnavy <krchnavy@cngroup.dk>
2014-07-09 21:52:49 +0200

    DPUConfig has again isValid()

--- dpu-api/src/main/java/eu/unifiedviews/dpu/config/DPUConfig.java
+++ dpu-api/src/main/java/eu/unifiedviews/dpu/config/DPUConfig.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * Base interface for DPU's configuration. <b>All the configuration objects must
  * provide public parameter-less constructor!</b>
- * 
+ *
  * @author Petyr
  */
 public interface DPUConfig extends Serializable {
@@ -20,5 +20,11 @@ public interface DPUConfig extends Serializable {
      * The method is called when ever the configuration is deserialized.
      */
     void onDeserialize();
+
+    /**
+     * @return True it he configuration is valid ie. can be used to configure
+     * DPU or configuration dialog.
+     */
+    boolean isValid();
 
 }
