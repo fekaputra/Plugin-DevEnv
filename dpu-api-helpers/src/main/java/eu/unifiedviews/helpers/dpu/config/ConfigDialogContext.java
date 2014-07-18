b6a0d9ecfb5a0ce7f8d620b383dc918e6c8915d4
Michal Klempa <mkl@eea.sk>
2014-07-18 14:54:03 +0200

    Adding dpu-api-helpers

---
+++ dpu-api-helpers/src/main/java/eu/unifiedviews/helpers/dpu/config/ConfigDialogContext.java
@@ -0,0 +1,15 @@
+package eu.unifiedviews.helpers.dpu.config;
+
+/**
+ * Context for {@link AbstractConfigDialog}.
+ * 
+ * @author Petyr
+ */
+public interface ConfigDialogContext {
+
+    /**
+     * @return True if the dialog belongs to template false otherwise
+     */
+    boolean isTemplate();
+
+}
