a6861a194e28cf714349294930005eeb507a5885
skodape <p.skoda@centrum.cz>
2014-08-01 11:50:37 +0200

    Copy helper copy data of first level ie ?root ?p ?o.

--- uv-dataunit-helpers/src/main/java/eu/unifiedviews/helpers/dataunit/copyhelper/CopyHelpers.java
+++ uv-dataunit-helpers/src/main/java/eu/unifiedviews/helpers/dataunit/copyhelper/CopyHelpers.java
@@ -3,11 +3,13 @@ package eu.unifiedviews.helpers.dataunit.copyhelper;
 import eu.unifiedviews.dataunit.DataUnitException;
 import eu.unifiedviews.dataunit.MetadataDataUnit;
 import eu.unifiedviews.dataunit.WritableMetadataDataUnit;
+import org.openrdf.model.URI;
 
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.Update;
 import org.openrdf.query.UpdateExecutionException;
+import org.openrdf.query.impl.DatasetImpl;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.slf4j.Logger;
@@ -37,6 +39,18 @@ public class CopyHelpers {
     }
 
     private class CopyHelperImpl implements CopyHelper {
+
+        protected static final String SYMBOLIC_NAME_BINDING = "symbolicName";
+        
+        /**
+         * Copy only first level.
+         */
+        protected static final String UPDATE = 
+                "INSERT {?s ?p ?o} WHERE {"
+                        + "?s ?p ?o."
+                        + "?s <http://unifiedviews.eu/DataUnit/MetadataDataUnit/symbolicName> ?" + SYMBOLIC_NAME_BINDING + "."
+                        + "}";
+
         private final Logger LOG = LoggerFactory.getLogger(CopyHelperImpl.class);
 
         private MetadataDataUnit source;
@@ -57,9 +71,23 @@ public class CopyHelpers {
                     connection = source.getConnection();
                 }
                 // Select all triples <bnode> symbolicName "symbolicName"
-                // add all of them to destination data unit (we use source connection - both run on same storage).
+                // add all of them to destination data unit
+                // (we use source connection - both run on same storage).
 
-                Update update = connection.prepareUpdate(QueryLanguage.SPARQL, "");
+                final Update update = connection.prepareUpdate(
+                        QueryLanguage.SPARQL, UPDATE);
+
+                update.setBinding(SYMBOLIC_NAME_BINDING, 
+                        connection.getValueFactory().createLiteral(symbolicName));
+
+                final DatasetImpl dataset = new DatasetImpl();
+                for (URI item : source.getMetadataGraphnames()) {
+                    dataset.addDefaultGraph(item);
+                }
+                dataset.setDefaultInsertGraph(
+                        destination.getMetadataWriteGraphname());
+
+                update.setDataset(dataset);
                 update.execute();
             } catch (RepositoryException | UpdateExecutionException | MalformedQueryException ex) {
                 throw new DataUnitException("", ex);
