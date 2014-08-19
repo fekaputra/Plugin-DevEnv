b56dc378e2c692726004470ab419b73b80d485e4
Michal Klempa <mkl@eea.sk>
2014-08-19 10:41:54 +0200

    Documenting files data unit

--- uv-dataunit-files/src/main/java/eu/unifiedviews/dataunit/files/FilesDataUnit.java
+++ uv-dataunit-files/src/main/java/eu/unifiedviews/dataunit/files/FilesDataUnit.java
@@ -1,18 +1,30 @@
 package eu.unifiedviews.dataunit.files;
 
-import eu.unifiedviews.dataunit.MetadataDataUnit;
 import eu.unifiedviews.dataunit.DataUnitException;
+import eu.unifiedviews.dataunit.MetadataDataUnit;
 
+/**
+ * Derived from {@link MetadataDataUnit}, {@link FilesDataUnit} stores metadata entries and each entry is enhanced by file location.
+ * This data unit stores files therefore, along with any metadata attached to file entries.
+ */
 public interface FilesDataUnit extends MetadataDataUnit {
 
+    /**
+     * Predicate used to store metadata about file (the URI location of the file) inside storage.
+     * Each metadata entry is then represented at least by triples:
+     * {@code
+     * <subject> p:symbolicName "name"
+     * <subject> p:fileUri <file://c:/Users/uv/some/location/main.xls>
+     * }
+     */
     static final String PREDICATE_FILE_URI = "http://unifiedviews.eu/DataUnit/MetadataDataUnit/FilesDataUnit/fileURI";
 
     interface Entry extends MetadataDataUnit.Entry {
 
         /**
+         * String stored URI representing real file location
          *
-         * @return URI (as string) of the real file location, for example:
-         * http://example.com/my_file.png or file://c:/Users/example/myDoc.doc
+         * @return URI (as string) of the real file location, for example: "http://example.com/my_file.png" or "file://c:/Users/example/myDoc.doc"
          * @throws eu.unifiedviews.dataunit.DataUnitException
          */
         String getFileURIString() throws DataUnitException;
@@ -30,5 +42,6 @@ public interface FilesDataUnit extends MetadataDataUnit {
      * @return
      * @throws DataUnitException
      */
+    @Override
     FilesDataUnit.Iteration getIteration() throws DataUnitException;
 }
