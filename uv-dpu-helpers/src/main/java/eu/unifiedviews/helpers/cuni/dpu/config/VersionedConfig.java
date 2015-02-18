f90c61af749f7fc9a8c53cc0e7f58ee539511ce3
skodape <p.skoda@centrum.cz>
2015-02-18 11:09:39 +0100

    Added main CUNI helpers project as uv-dpu-heleprs.

---
+++ uv-dpu-helpers/src/main/java/eu/unifiedviews/helpers/cuni/dpu/config/VersionedConfig.java
@@ -0,0 +1,20 @@
+package eu.unifiedviews.helpers.cuni.dpu.config;
+
+import eu.unifiedviews.dpu.config.DPUConfigException;
+
+/**
+ * Interface for versioned configuration.
+ *
+ * @author Škoda Petr
+ * @param <CONFIG> Configuration to which this configuration can be converted (updated) to.
+ */
+public interface VersionedConfig<CONFIG> {
+
+    /**
+     *
+     * @return Next version of configuration with "same" settings.
+     * @throws DPUConfigException
+     */
+    CONFIG toNextVersion() throws DPUConfigException;
+
+}
